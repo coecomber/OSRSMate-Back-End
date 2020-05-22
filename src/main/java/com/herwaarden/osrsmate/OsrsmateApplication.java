2aa85113e687c30e9e80ac761a7f4518886bdac1
coecomber <joostvherwaarden@hotmail.com>
2020-05-22 23:23:48 +0200

    Init spring boot project

---
+++ src/main/java/com/herwaarden/osrsmate/OsrsmateApplication.java
@@ -0,0 +1,13 @@
+package com.herwaarden.osrsmate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OsrsmateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OsrsmateApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/herwaarden/osrsmate/OsrsmateApplicationTests.java
@@ -0,0 +1,13 @@
+package com.herwaarden.osrsmate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OsrsmateApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
